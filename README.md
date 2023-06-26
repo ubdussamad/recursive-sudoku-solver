53879a70d5fc30693bfd92a8604234afcdba0ea0
Samad Haque <samad@SamadsMcbookPro.hsd1.va.comcast.net>
2023-06-26 08:52:26 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# recursive-sudoku-solver
---
+++ hackerman.py
@@ -0,0 +1,24 @@
+import math
+import time
+import random
+import sys
+import os
+
+
+def read_file(filename):
+    pass
+
+def solve(matrix):
+    pass
+
+def print_matrix(matrix):
+    pass
+
+def main():
+    # Read the file
+    # Solve the problem
+    # Print the solution
+    pass
+
+if __name__ == "__main__":
+    main()
