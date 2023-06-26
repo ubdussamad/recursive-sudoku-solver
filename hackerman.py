172560befa878bb98b46d77f6d25a41b5a1f3f66
Samad (Sam) <ubdussamad@gmail.com>
2023-06-26 09:11:32 -0400

    Update hackerman.py

--- hackerman.py
+++ hackerman.py
@@ -8,9 +8,15 @@ import os
 def read_file(filename):
     pass
 
+# This function could be the recursive one or you can make others.
 def solve(matrix):
     pass
 
+# Given a cell's index (row, col) in the 2D array, it'll return all possible numbers which can be filled in that cell.
+def list_possibilities(row_index,col_index):
+    pass
+
+# Just to take a look at the matrix before and after it's solved.
 def print_matrix(matrix):
     pass
 
